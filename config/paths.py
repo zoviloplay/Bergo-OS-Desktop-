1c98406956cc3cf620da52c6f1874c44cf22a443
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:27:52 +0200

    Update paths.py

--- config/paths.py
+++ config/paths.py
@@ -1,1 +1,9 @@
+import os
 
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+SETTINGS = os.path.join(BASE, "settings.json")
+SYSTEM = os.path.join(BASE, "system.json")
+DESKTOP = os.path.join(BASE, "desktop.json")
+TERMINAL = os.path.join(BASE, "terminal.json")
+UPDATE = os.path.join(BASE, "update.json")
