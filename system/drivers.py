3abdf52c5301e9df31342dc26276e86afbd25cee
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:35:47 +0200

    Update drivers.py

--- system/drivers.py
+++ system/drivers.py
@@ -1,1 +1,8 @@
+def load_all():
+    print("Lade Treiber...")
 
+    print("GPU-Treiber geladen")
+    print("Netzwerk-Treiber geladen")
+    print("Input-Treiber geladen")
+
+    print("Alle Treiber erfolgreich geladen.")
