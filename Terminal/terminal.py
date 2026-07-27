07ba03fd55b7b02146e2b5f1856d24094160facb
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:09:31 +0200

    Update terminal.py

--- Terminal/terminal.py
+++ Terminal/terminal.py
@@ -1,1 +1,8 @@
+import parser
 
+def start():
+    print("Bergo Terminal gestartet. Tippe 'help' für Befehle.")
+
+    while True:
+        user_input = input("> ")
+        parser.handle(user_input)
