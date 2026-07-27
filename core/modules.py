ee48003b9977bb5a56a7374de00d6a2a0eb0cc25
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:02:27 +0200

    Update modules.py

--- core/modules.py
+++ core/modules.py
@@ -1,1 +1,8 @@
+def load_all():
+    print("Lade Module...")
 
+    print("Desktop geladen")
+    print("Terminal geladen")
+    print("Installer geladen")
+
+    print("Module erfolgreich geladen.")
