b58e3a13e57a0d945d5d0778aa8141c3695d3c7f
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:15:23 +0200

    Update parser.py

--- Terminal/parser.py
+++ Terminal/parser.py
@@ -1,1 +1,13 @@
+import commands
 
+def handle(cmd):
+    if cmd == "help":
+        commands.help()
+    elif cmd == "info":
+        commands.info()
+    elif cmd == "clear":
+        commands.clear()
+    elif cmd == "exit":
+        commands.exit_terminal()
+    else:
+        print("Unbekannter Befehl:", cmd)
