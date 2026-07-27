146a357af11d449331547f789e2af786a15b710d
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 19:41:44 +0200

    Update boot.py

--- system/boot.py
+++ system/boot.py
@@ -1,4 +1,17 @@
 import system.kernel_loader as kernel
+import time
+import os
 
 print("Starte Bergo OS...")
 kernel.start_kernel()
+
+# FAST Boot Animation
+os.system("clear")
+print("[LOGO] Fade-In")
+time.sleep(0.5)
+print("[LOGO] Glow-Effekt")
+time.sleep(1)
+print("[LOGO] Fade-Out")
+time.sleep(0.5)
+print("Desktop wird geladen...")
+time.sleep(0.5)
