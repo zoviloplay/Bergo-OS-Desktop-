b117deda8e5a65c58016bca94f1a53f06b472baa
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:34:53 +0200

    Update kernel_loader.py

--- system/kernel_loader.py
+++ system/kernel_loader.py
@@ -1,1 +1,12 @@
+import boot_sequence
+import hardware_check
+import drivers
 
+def start_kernel():
+    print("Starte Bergo Kernel...")
+
+    hardware_check.run()
+    drivers.load_all()
+    boot_sequence.run()
+
+    print("Kernel erfolgreich gestartet.")
