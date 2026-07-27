34814bf51124c5761d29d11c2c2ad133bc5dccf8
zoviloplay <jonastropenstein@gmail.com>
2026-07-27 11:35:17 +0200

    Update hardware_check.py

--- system/hardware_check.py
+++ system/hardware_check.py
@@ -1,1 +1,9 @@
+def run():
+    print("Hardwarecheck läuft...")
 
+    print("CPU OK")
+    print("RAM OK")
+    print("GPU OK")
+    print("Speicher OK")
+
+    print("Hardwarecheck abgeschlossen.")
