b0208b015abc1603795d2f99120bbc6d0790f325
zoviloplay <jonastropenstein@gmail.com>
2026-07-28 14:28:40 +0200

    Update input_loader.py

--- system/input_loader.py
+++ system/input_loader.py
@@ -1,1 +1,9 @@
+import os
 
+print("🔌 Aktiviere Eingabegeräte...")
+
+os.system("modprobe usbhid")
+os.system("modprobe hid-generic")
+os.system("modprobe evdev")
+
+print("🖱️ Maus & ⌨️ Tastatur aktiv!")
